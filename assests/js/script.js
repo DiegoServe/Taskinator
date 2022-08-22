8b3cfb3cc9ef3afc3e83d05a1d5cb883e8296478
Diego Conserve <diegoconserve@MacBook-Pro.local>
2022-08-22 17:12:44 -0400

    handle form submissions

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function() { 
@@ -8,4 +8,17 @@ var createTaskHandler = function() {
   tasksToDoEl.appendChild(listItemEl); 
 }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+  
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+  
+    console.dir(listItemEl);
+
+formEl.addEventListener("submit", createTaskHandler);
